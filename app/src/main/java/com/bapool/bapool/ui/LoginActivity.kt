b30d7072a5fd16da2849377b47ad5d513ad06f54
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2023-03-17 13:37:45 +0900

    불필요한 카카오 모듈 삭제 레트로핏 라이브러리 구현 레트로핏 데이터 클래스 생성 마커 구현 (뫀서버 이용) 현위치에서 재검색(FAB) 기능 구현

--- app/src/main/java/com/bapool/bapool/ui/LoginActivity.kt
+++ app/src/main/java/com/bapool/bapool/ui/LoginActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import android.widget.ImageButton
 import android.widget.Toast
 import com.bapool.bapool.R
-import com.bapool.bapool.kakaoUser
+import com.bapool.bapool.RetrofitService
 import com.bapool.bapool.retrofit.data.accessToken
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
@@ -83,7 +83,7 @@ class LoginActivity : AppCompatActivity() {
                     Retrofit.Builder()
                         .baseUrl("https://655c8626-5f5d-4846-b60c-20c52d2ea0da.mock.pstmn.io")//baseurl
                         .addConverterFactory(GsonConverterFactory.create()).build()
-                val service = retrofit.create(kakaoUser::class.java)
+                val service = retrofit.create(RetrofitService::class.java)
 
                 service.gettoken(token).enqueue(object : Callback<accessToken> {
                     override fun onResponse(
