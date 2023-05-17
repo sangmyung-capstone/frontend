eeadd90ea4053feeb145cfb0ca6a66967765d9f4
sonsh0417 <okgnb@naver.com>
2023-05-17 14:07:39 +0900

    create PartyInfoViewModel

---
+++ app/src/main/java/com/bapool/bapool/retrofit/data/PartyInfoViewModel.kt
@@ -0,0 +1,19 @@
+package com.bapool.bapool.retrofit.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PartyInfoViewModel : ViewModel() {
+    private var sharedObjectInfo = MutableLiveData<FirebasePartyInfo>()
+
+
+    fun setObjectInfo(newObjectInfo: FirebasePartyInfo) {
+        sharedObjectInfo.value = newObjectInfo
+    }
+
+    fun getObjectInfo(): LiveData<FirebasePartyInfo> {
+        return sharedObjectInfo
+    }
+
+}
